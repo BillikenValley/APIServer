3073ae9c9e65e64ad74fd0368b7fbde60cb5b0a5
avikejriwal <avi.kejriwal@gmail.com>
2016-10-22 22:08:35 -0500

    added homeless shelters

--- model/shelters.go
+++ model/shelters.go
@@ -12,7 +12,7 @@ func init() {
 			Location: Location{
 				Address: "1000 N. 19th St.\nSt.Louis, MO 63135",
 				Lat:     38.6376,
-				Lng:     90.2047,
+				Lng:     -90.2047,
 			},
 			ShelterConstraints: map[string]int{
 				AcceptsSingleMen:    0,
@@ -36,7 +36,7 @@ func init() {
 			Location: Location{
 				Address: "4223 S. Compton    St. Louis, MO 6311",
 				Lat:     38.5799,
-				Lng:     90.2403,
+				Lng:     -90.2403,
 			},
 			ShelterConstraints: map[string]int{
 				AcceptsSingleMen:    0,
@@ -60,9 +60,9 @@ func init() {
 			Name: "Loaves & Fishes Inc.",
 			Beds: make([]int, 21),
 			Location: Location{
-				Address: "2750 McKelvey Rd, Maryland Heights, MO 63043",
-				Lat:     38.7333,
-				Lng:     90.4457,
+				Address: "",
+				Lat:     38.607488,
+				Lng:     -90.203525
 			},
 			ShelterConstraints: map[string]int{
 				AcceptsSingleMen:       0,
@@ -73,7 +73,138 @@ func init() {
 				MustHaveSSN:            0,
 				AcceptsSexOffenders:    0,
 				AcceptsViolentCriminal: 0,
-				TransFriendly:          0,
+				TransFriendly:          1,
+
+			},
+			OpenTime:    time.Now(),
+			ClosingTime: time.Now().Add(2 * time.Hour),
+			ShelterCredentials: ShelterCredentials{
+				Username: "david",
+				Password: "password",
+			},
+		},
+		3: Shelter{
+			Name: "Peter & Paul",
+			Beds: make([]int, 60),
+			Location: Location{
+				Address: "711 Allen Ave, St. Louis, MO 63104",
+				Lat:     38.607303,
+				Lng:    -90.203450,
+			},
+			ShelterConstraints: map[string]int{
+				AcceptsSingleMen:       1,
+				AcceptsSingleWomen:     0,
+				AcceptsChildren:        0,
+				AcceptsDrunks:          1,
+				MustHaveID:             0,
+				MustHaveSSN:            0,
+				AcceptsSexOffenders:    1,
+				AcceptsViolentCriminal: 0,
+				TransFriendly:          1,
+			},
+			OpenTime:    time.Now(),
+			ClosingTime: time.Now().Add(2 * time.Hour),
+			ShelterCredentials: ShelterCredentials{
+				Username: "david",
+				Password: "password",
+			},
+		},
+		4: Shelter{
+			Name: "Room at The Inn",
+			Beds: make([]int, 20),
+			Location: Location{
+				Address: "3415 Bridgeland Dr, Bridgeton, MO 63044",
+				Lat:     38.746497,
+				Lng:     -90.429620
+			},
+			ShelterConstraints: map[string]int{
+				AcceptsSingleMen:       0,
+				AcceptsSingleWomen:     1,
+				AcceptsChildren:        1,
+				AcceptsDrunks:          1,
+				MustHaveID:             0,
+				MustHaveSSN:            0,
+				AcceptsSexOffenders:    0,
+				AcceptsViolentCriminal: 0,
+				TransFriendly:          1,
+			},
+			OpenTime:    time.Now(),
+			ClosingTime: time.Now().Add(2 * time.Hour),
+			ShelterCredentials: ShelterCredentials{
+				Username: "david",
+				Password: "password",
+			},
+		},
+		5: Shelter{
+			Name: "Saint Patrick Center - Women's night",
+			Beds: make([]int, 20),
+			Location: Location{
+				Address: "800 N Tucker Blvd, St. Louis, MO 63101",
+				Lat:     38.633424,
+				Lng:     -90.195435
+			},
+			ShelterConstraints: map[string]int{
+				AcceptsSingleMen:       0,
+				AcceptsSingleWomen:     1,
+				AcceptsChildren:        0,
+				AcceptsDrunks:          1,
+				MustHaveID:             0,
+				MustHaveSSN:            0,
+				AcceptsSexOffenders:    1,
+				AcceptsViolentCriminal: 0,
+				TransFriendly:          1,
+			},
+			OpenTime:    time.Now(),
+			ClosingTime: time.Now().Add(2 * time.Hour),
+			ShelterCredentials: ShelterCredentials{
+				Username: "david",
+				Password: "password",
+			},
+		},
+		6: Shelter{
+			Name: "Salvation Army Family Haven",
+			Beds: make([]int, 64),
+			Location: Location{
+				Address: "10740 Page Ave, St. Louis, MO 63132",
+				Lat:     38.692118,
+				Lng:     -90.398464
+			},
+			ShelterConstraints: map[string]int{
+				AcceptsSingleMen:       1,
+				AcceptsSingleWomen:     1,
+				AcceptsChildren:        1,
+				AcceptsDrunks:          1,
+				MustHaveID:             0,
+				MustHaveSSN:            0,
+				AcceptsSexOffenders:    0,
+				AcceptsViolentCriminal: 0,
+				TransFriendly:          1,
+			},
+			OpenTime:    time.Now(),
+			ClosingTime: time.Now().Add(2 * time.Hour),
+			ShelterCredentials: ShelterCredentials{
+				Username: "david",
+				Password: "password",
+			},
+		},
+		7: Shelter{
+			Name: "12th and Park Shelter",
+			Beds: make([]int, 125),
+			Location: Location{
+				Address: "1410 S Tucker Blvd, St. Louis, MO 63104",
+				Lat:     38.614828,
+				Lng:     -90.203185
+			},
+			ShelterConstraints: map[string]int{
+				AcceptsSingleMen:       1,
+				AcceptsSingleWomen:     0,
+				AcceptsChildren:        0,
+				AcceptsDrunks:          1,
+				MustHaveID:             0,
+				MustHaveSSN:            0,
+				AcceptsSexOffenders:    0,
+				AcceptsViolentCriminal: 0,
+				TransFriendly:          1,
 			},
 			OpenTime:    time.Now(),
 			ClosingTime: time.Now().Add(2 * time.Hour),
